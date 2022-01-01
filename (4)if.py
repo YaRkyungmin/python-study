4ff08529d77fdfe8a9c8359fedf2a8f3a0251527
KANGKYUNGMIN <kkgm94@naver.com>
2022-01-01 23:53:47 +0900

    add explain

--- (4)if.py
+++ (4)if.py
@@ -6,4 +6,14 @@
 print(type(True)) # 0이 아닌 수, "abc", [1,2,3,..]
 print(type(False)) # 0, '', [], (), {}
 
+ex = []
+if ex:
+    print("success")
+else:
+    print("fail")
 
+ex2 = [0]
+if ex2:
+    print(type(ex2),"success")
+else:
+    print("fail")
