4ab17fafa493f966ba2338f176414770689beef2
KANGKYUNGMIN <kkgm94@naver.com>
2022-01-02 00:02:39 +0900

    add explain

--- (4)if.py
+++ (4)if.py
@@ -17,3 +17,20 @@ if ex2:
     print(type(ex2),"success")
 else:
     print("fail")
+
+ex3 = ()
+if ex and ex2:
+    print("success")
+    print(not True)
+elif ex or ex3:
+    print("success2")
+elif not ex3:
+    print("success3")
+    print(not False)
+else:
+    print("fail")
+
+## priority
+'''
+산술 > 관계 > 논리
+'''
